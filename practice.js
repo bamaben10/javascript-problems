b1047dec0e55e7803badd9277ce0ebf0b9b6d79b
Benjamin Salmon <bensalmon8@gmail.com>
2023-01-10 16:31:45 -0600

    practice

--- practice.js
+++ practice.js
@@ -1,3 +1,11 @@
+// Given the starting code of:
+// x = 1
+// y = 2
+// Write a block of code that will print out the largest value.
+// Using the given values as an example, your code should print out 2.
+// To verify your code is working properly, try and change the values of x and y.
+// As an additional challenge, print out The values are identical. if x and y have the same value.
+
 let x = 1;
 let y = 2;
 
@@ -6,5 +14,5 @@ if (x > y) {
 } else if (y > x) {
   console.log(y);
 } else {
-  console.log("The vales are identical");
+  console.log("The values are identical");
 }
