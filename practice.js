8561b8ae8a7fce6a9da3c7c2998092cc868785ce
Benjamin Salmon <bensalmon8@gmail.com>
2023-02-02 20:34:37 -0600

    practice

--- practice.js
+++ practice.js
@@ -17,13 +17,13 @@ if (x > y) {
   console.log("The values are identical");
 }
 
-let x = 1;
-let y = 2;
+// let x = 1;
+// let y = 2;
 
-if (x > y) {
-  console.log(x);
-} else if (y > x) {
-  console.log(y);
-} else {
-  console.log("The values are identical");
-}
+// if (x > y) {
+//   console.log(x);
+// } else if (y > x) {
+//   console.log(y);
+// } else {
+//   console.log("The values are identical");
+// }
