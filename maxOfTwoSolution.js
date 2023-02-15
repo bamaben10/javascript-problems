a67b41a400ba944ff0bfb07bd8fd03d5b2897fd3
Benjamin Salmon <bensalmon8@gmail.com>
2023-02-14 19:18:37 -0600

    practice

--- maxOfTwoSolution.js
+++ maxOfTwoSolution.js
@@ -1,20 +1,32 @@
-let x = 1
-let y = 2
+// Given the starting code of:
+
+// x = 1
+// y = 2
+// Write a block of code that will print out the largest value.
+
+// Using the given values as an example, your code should print out 2.
+
+// To verify your code is working properly, try and change the values of x and y.
+
+// As an additional challenge, print out The values are identical. if x and y have the same value.
+
+let x = 2;
+let y = 2;
 
 // Write your code below this line.
 
 if (x > y) {
-  console.log(x)
+  console.log(x);
+} else if (y > x) {
+  console.log(y);
 } else {
-  console.log(y)
+  console.log("the values are identical");
 }
 
 // Solution to additional challenge:
 
-if (x > y) {
-  console.log(x)
-} else if (y > x) {
-  console.log(y)
-} else [
-  console.log('The values are identical.')
-]
+// if (x > y) {
+//   console.log(x);
+// } else if (y > x) {
+//   console.log(y);
+// } else [console.log("The values are identical.")];
